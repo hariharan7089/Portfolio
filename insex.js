6bab2e27ea899bd199c11e4e2c04061d675e5d05
hariharan7089 <hariharank007089@gmail.com>
2023-10-27 10:49:00 +0530

    add

---
+++ insex.js
@@ -0,0 +1,26 @@
+let input=document.getElementById("input");
+let list=document.getElementById("list")
+let button=document.getElementById("button")
+button.addEventListener("click",add);
+function add(){
+  const item=input.value ;
+  input.value="";
+   const li=document.createElement("li");
+   li.innerHTML= `${item} <i></i>`;
+   li.addEventListener("click",()=>{
+  li.classList.toggle('done')
+   })
+   list.appendChild(li);
+   li.querySelector("i").addEventListener("click",()=>{
+    li.remove();
+   });
+}
+const btn=document.querySelector(".button");
+btn.addEventListener("click", () => {
+  const liItems = document.querySelectorAll("li");
+  liItems.forEach((li) => {
+    li.remove();
+  });
+});
+
+
