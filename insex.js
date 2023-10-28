412539df7c972116b97c211999474fcb974054de
hariharan7089 <hariharank007089@gmail.com>
2023-10-28 21:44:45 +0530

    add

--- insex.js
+++ insex.js
@@ -1,19 +1,21 @@
 let input=document.getElementById("input");
 let list=document.getElementById("list")
 let button=document.getElementById("button")
-button.addEventListener("click",add);
 function add(){
-  const item=input.value ;
-  input.value="";
-   const li=document.createElement("li");
-   li.innerHTML= `${item} <i></i>`;
-   li.addEventListener("click",()=>{
-  li.classList.toggle('done')
-   })
-   list.appendChild(li);
-   li.querySelector("i").addEventListener("click",()=>{
-    li.remove();
-   });
+  if(input.value){
+    const item=input.value ;
+    input.value="";
+     const li=document.createElement("li");
+     li.innerHTML= `${item} <i></i>`;
+     li.addEventListener("click",()=>{
+    li.classList.toggle('done')
+     })
+     list.appendChild(li);
+     li.querySelector("i").addEventListener("click",()=>{
+      li.remove();
+     });
+  }
+ 
 }
 const btn=document.querySelector(".button");
 btn.addEventListener("click", () => {
